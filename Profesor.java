add3a3e72872dca50a64f8c179ad351058d8abda
unknown <miguel112811@gmail.com>
2023-07-31 21:40:46 -0600

    Se completaron los metodos de Profesor y Secretaria

--- Profesor.java
+++ Profesor.java
@@ -5,12 +5,15 @@ public class Profesor extends Docentes{
     public Profesor(String nombre, String genero, int edad, double sueldo){
         super(nombre, genero, edad, sueldo);
     }
-    public void promedioGrupal(){
-        System.out.println("El promedio de las calificaciones de los alumnos es: "+ promedioGrupal);
-        
+    public void promedioGrupal(String nombre1,String nombre2,String nombre3,double promedioAlu1,double promedioAlu2, double promedioAlu3){
+        System.out.println("Las calificaciones de sus alumnos son:");
+        System.out.println(nombre1 +" : " + promedioAlu1);
+        System.out.println(nombre2 +" : " + promedioAlu2);
+        System.out.println(nombre3 +" : " + promedioAlu3);
     }
-    public double Evaluar(){
+    public double Evaluar(double promedioAlu1,double promedioAlu2, double promedioAlu3){
         System.out.println("El promedio de su grupo es :");
+        promedioGrupal = promedioAlu1 + promedioAlu2 + promedioAlu3;
         return promedioGrupal;
     }
 
--- Secretaria.java
+++ Secretaria.java
@@ -1,15 +1,16 @@
 
 
 public class Secretaria extends Docentes{
+    
 
     public Secretaria(String nombre, String genero, int edad, double sueldo){
         super(nombre, genero, edad, sueldo);
     }
-    public void Pagar(){
+    public void Pagar(double sueldoProfesor,double sueldoIntendente, double sueldoDirector){
         System.out.println("El sueldo de la secretaria es; " + sueldo);
-        System.out.println("Se paga: "+  + "Al intendente" + );
-        System.out.println("Se paga: "+  + "Al Director");
-         System.out.println("Se paga: "+  + "Al profesor");
+        System.out.println("Se paga: "+ sueldoIntendente + " Al intendente");
+        System.out.println("Se paga: "+  sueldoDirector + " Al Director");
+        System.out.println("Se paga: "+ sueldoProfesor + " Al profesor");
     }
 }
 
